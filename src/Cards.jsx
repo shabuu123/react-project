3a198a5ccec18162a854a097efd2a7f5f53dda0a
Sabnam <sabnamchoudhary10@gmail.com>
2024-11-22 14:36:44 +0530

    pushing react-project

---
+++ src/Cards.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+function Cards (props){
+return(
+    <div>{props.name}</div>
+)
+}
+
+export default Cards
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
+import './index.css'
 import './style.css'
-import Greeting from './Greeting.jsx'
+// import Greeting from './Greeting.jsx'
+import Cards from './Cards.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Greeting/>
+    {/* <Greeting/> */}
+    <Cards/>
   </StrictMode>,
 )
